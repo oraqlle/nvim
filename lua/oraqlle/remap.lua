776423b026bb2ad21f2d64c46d77bfb776f840f9
oraqlle <41113853+oraqlle@users.noreply.github.com>
2024-01-16 10:19:16 +1100

    Added format remap

--- lua/oraqlle/remap.lua
+++ lua/oraqlle/remap.lua
@@ -1,6 +1,8 @@
-
 -- Leader key for custom commands/remaps
 vim.g.mapleader = " "
 
 -- " pv" opens file explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
