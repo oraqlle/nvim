2d93d886077a3c61bf80a8529fdc66c9465a0c8d
oraqlle <41113853+oraqlle@users.noreply.github.com>
2024-06-12 14:54:03 +1000

    Formatting to packer file

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -26,6 +26,7 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
+    disable = { "csv" },  -- List of languages to disable treesitter for.
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
--- lua/oraqlle/packer.lua
+++ lua/oraqlle/packer.lua
@@ -1,75 +1,95 @@
- vim.cmd [[packadd packer.nvim]]
+vim.cmd [[packadd packer.nvim]]
 
- return require('packer').startup(function(use)
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-     -- Packer can manage itself
-     use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-     use {
-         'nvim-telescope/telescope.nvim', tag = '0.1.4',
-         requires = { {'nvim-lua/plenary.nvim'} }
-     }
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
 
-     use({
-         'rose-pine/neovim',
-         as = 'rose-pine',
-         config = function()
-             vim.cmd('colorscheme rose-pine')
-         end
-     })
+    use({
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup {
+                icons = false,
+            }
+        end
+    })
 
-     use({
-         "folke/trouble.nvim",
-         config = function()
-             require("trouble").setup {
-                 icons = false,
-             }
-         end
-     })
+    -- use 'wakatime/vim-wakatime'
 
-     use 'wakatime/vim-wakatime'
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end, }
+        use("nvim-treesitter/playground")
+        use("theprimeagen/harpoon")
+        use("theprimeagen/refactoring.nvim")
+        use("mbbill/undotree")
+        use("tpope/vim-fugitive")
+        use("nvim-treesitter/nvim-treesitter-context");
 
-     use {
-         'nvim-treesitter/nvim-treesitter',
-         run = function()
-             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-             ts_update()
-         end,}
-         use("nvim-treesitter/playground")
-         use("theprimeagen/harpoon")
-         use("theprimeagen/refactoring.nvim")
-         use("mbbill/undotree")
-         use("tpope/vim-fugitive")
-         use("nvim-treesitter/nvim-treesitter-context");
+        use {
+            'VonHeikemen/lsp-zero.nvim',
+            branch = 'v1.x',
+            requires = {
+                -- LSP Support
+                { 'neovim/nvim-lspconfig' },
+                { 'williamboman/mason.nvim' },
+                { 'williamboman/mason-lspconfig.nvim' },
 
-         use {
-             'VonHeikemen/lsp-zero.nvim',
-             branch = 'v1.x',
-             requires = {
-                 -- LSP Support
-                 {'neovim/nvim-lspconfig'},
-                 {'williamboman/mason.nvim'},
-                 {'williamboman/mason-lspconfig.nvim'},
+                -- Autocompletion
+                { 'hrsh7th/nvim-cmp' },
+                { 'hrsh7th/cmp-buffer' },
+                { 'hrsh7th/cmp-path' },
+                { 'saadparwaiz1/cmp_luasnip' },
+                { 'hrsh7th/cmp-nvim-lsp' },
+                { 'hrsh7th/cmp-nvim-lua' },
 
-                 -- Autocompletion
-                 {'hrsh7th/nvim-cmp'},
-                 {'hrsh7th/cmp-buffer'},
-                 {'hrsh7th/cmp-path'},
-                 {'saadparwaiz1/cmp_luasnip'},
-                 {'hrsh7th/cmp-nvim-lsp'},
-                 {'hrsh7th/cmp-nvim-lua'},
+                -- Snippets
+                { 'L3MON4D3/LuaSnip' },
+            }
+        }
 
-                 -- Snippets
-                 {'L3MON4D3/LuaSnip'},
-             }
-         }
+        use("folke/zen-mode.nvim")
+        use("eandrju/cellular-automaton.nvim")
+        use("laytan/cloak.nvim")
 
-         use("folke/zen-mode.nvim")
-         use("eandrju/cellular-automaton.nvim")
-         use("laytan/cloak.nvim")
+        use {
+            'jakemason/ouroboros',
+            requires = { { 'nvim-lua/plenary.nvim' } }
+        }
 
-         use {
-             'jakemason/ouroboros',
-             requires = { {'nvim-lua/plenary.nvim'} }
-         }
-     end)
+        use {
+            'cameron-wags/rainbow_csv.nvim',
+            config = function()
+                require 'rainbow_csv'.setup()
+            end,
+            -- optional lazy-loading below
+            module = {
+                'rainbow_csv',
+                'rainbow_csv.fns'
+            },
+            ft = {
+                'csv',
+                'tsv',
+                'csv_semicolon',
+                'csv_whitespace',
+                'csv_pipe',
+                'rfc_csv',
+                'rfc_semicolon'
+            }
+        }
+    end)
